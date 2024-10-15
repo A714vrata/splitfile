0e9dd537d765e43955abe151834c15c66351e36e
mhtnmiro <mhtnmiro@tutanota.com>
2024-10-15 22:20:27 +0530

    Free memory upon failure

--- split_file.c
+++ split_file.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define FNAME 50
-#define LNBUF 255
-#define TXBUF 5000
+#define SIZE_FN 50
+#define SIZE_LNBUF 255
+#define SIZE_TXBUF 5000
 
 int main() {
   int bufsize = 0;
   int runs = 0;
 
-  char filename[FNAME] = "";
-  char lnbuf[LNBUF] = "";
+  char filename[SIZE_FN] = "";
+  char lnbuf[SIZE_LNBUF] = "";
 
-  char *txbuf = malloc(TXBUF);
+  char *txbuf = malloc(SIZE_TXBUF);
   if (txbuf == NULL) {
     fprintf(stderr, "Memory allocation failed\nExiting\n");
     return EXIT_FAILURE;
@@ -24,6 +24,7 @@ int main() {
   FILE *readpoint = fopen(filename, "r");
   if (readpoint == NULL) {
     fprintf(stderr, "Failed to open file %s\nExiting\n", filename);
+    free(txbuf);
     return EXIT_FAILURE;
   }
 
@@ -31,22 +32,23 @@ int main() {
     bufsize = 0;
     txbuf[0] = '\0';
 
-    while (bufsize < TXBUF && fgets(lnbuf, LNBUF, readpoint)) {
-      if (bufsize + strlen(lnbuf) >= TXBUF) {
+    while (bufsize < SIZE_TXBUF && fgets(lnbuf, SIZE_LNBUF, readpoint)) {
+      if (bufsize + strlen(lnbuf) >= SIZE_TXBUF) {
         break;
       }
       strcat(txbuf, lnbuf);
       bufsize += strlen(lnbuf);
     }
-    snprintf(filename, FNAME, "section%d.txt", runs + 1);
+    snprintf(filename, SIZE_FN, "section%d.txt", runs + 1);
     FILE *writepoint = fopen(filename, "w");
     if (writepoint == NULL) {
       fprintf(stderr,
               "Failed to create file %s\nPrinting to output instead\n\n\n",
               filename);
       printf("%s", txbuf);
+    } else {
+      fprintf(writepoint, "%s", txbuf);
     }
-    fprintf(writepoint, "%s", txbuf);
     fclose(writepoint);
     runs++;
   }
