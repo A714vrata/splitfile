2bf0da1f9e4cff8c82cf1aad4d7e51c78b197c65
aryavrata <aryavrata@tutanota.com>
2026-05-22 14:07:32 +0530

    Add comment
    Add comments to document code behaviour

--- splitfile.c
+++ splitfile.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define SIZE_FN 50
-#define SIZE_LNBUF 255
-#define SIZE_TXBUF 5000
+#define SIZE_FN 50 //maxsize for output filename
+#define SIZE_LNBUF 255 //line buffer max size
+#define SIZE_TXBUF 5000 //text file buffer for each split file
 
 int main() {
   int bufsize = 0;
@@ -12,7 +12,7 @@ int main() {
   char filename[SIZE_FN] = "";
   char lnbuf[SIZE_LNBUF] = "";
 
-  char *txbuf = malloc(SIZE_TXBUF);
+  char *txbuf = malloc(SIZE_TXBUF); //allocate split file buffer 
   if (txbuf == NULL) {
     fprintf(stderr, "Memory allocation failed\nExiting\n");
     return EXIT_FAILURE;
@@ -21,7 +21,7 @@ int main() {
   printf("Enter file name to be split: ");
   scanf("%s", filename);
 
-  FILE *readpoint = fopen(filename, "r");
+  FILE *readpoint = fopen(filename, "r"); //open fstream, read input file
   if (readpoint == NULL) {
     fprintf(stderr, "Failed to open file %s\nExiting\n", filename);
     free(txbuf);
@@ -33,6 +33,8 @@ int main() {
     bufsize = 0;
     txbuf[0] = '\0';
 
+    //read to buffer until 5000 character limit is reached
+
     while (bufsize < SIZE_TXBUF && fgets(lnbuf, SIZE_LNBUF, readpoint)) {
       if (bufsize + strlen(lnbuf) >= SIZE_TXBUF) {
         break;
@@ -40,6 +42,10 @@ int main() {
       strcat(txbuf, lnbuf);
       bufsize += strlen(lnbuf);
     }
+    /* split file into sections
+     * create new file per section
+     * 5000 char limit per split file
+     */
     snprintf(filename, SIZE_FN, "section%d.txt", runs + 1);
     FILE *writepoint = fopen(filename, "w");
     if (writepoint == NULL) {
@@ -48,7 +54,7 @@ int main() {
               filename);
       printf("%s", txbuf);
     } else {
-      fprintf(writepoint, "%s", txbuf);
+      fprintf(writepoint, "%s", txbuf); // Write file buffer to split file "section"
     }
     fclose(writepoint);
     runs++;
